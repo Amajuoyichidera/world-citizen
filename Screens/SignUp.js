5e03953fd0e52b3a5b24d2e5787cd14d15b60995
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-04-12 11:37:12 -0700

    first version

--- Screens/SignUp.js
+++ Screens/SignUp.js
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View , Image, Pressable, TextInput} from 'react-native';
+import { StyleSheet, Text, View , Image, TouchableOpacity, Pressable, TextInput} from 'react-native';
 import React from 'react';
 import { useState } from 'react';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { useTogglePasswordVisibility } from '../Screens/useTogglePasswordVisibility';
-import { SelectList } from 'react-native-dropdown-select-list'
+import { SelectList } from 'react-native-dropdown-select-list';
+import Checkbox from 'expo-checkbox';
 
 
 
@@ -23,7 +24,7 @@ export default function Find({ navigation }) {
       {key:'5', value:'U.K'},
       {key:'6', value:'U.S.A'},
       {key:'7', value:'London'},
-  ]
+  ];
   
   const [gender, setGender] = useState("");
   
@@ -31,13 +32,15 @@ export default function Find({ navigation }) {
       {key:'1', value:'Male'},
       {key:'2', value:'Female'},
       {key:'3', value:'Prefer Not To Say'},
-  ]
+  ];
+
+  const [agree, setAgree] = useState(false);
 
 return (
-  <View>
+  <View style={styles.all}>
       <View>
-          <Text>WELCOME</Text>
-          <Text>We are exited you are here, kindly fil in your details below!</Text>
+          <Text style={styles.well1}>WELCOME</Text>
+          <Text style={styles.well}>We are exited you are here, kindly fil in {'\n'} your details below!</Text>
       </View>
 
       <View  style={styles.mail}>
@@ -45,14 +48,17 @@ return (
              style={styles.input}
               placeholder="Name"
               keyboardType= 'default'
+              placeholderTextColor = "#0096C7"
               />        
   
           <TextInput
              style={styles.input}
               placeholder="Email"
               keyboardType= 'default'
+              placeholderTextColor = "#0096C7"
               />
-          
+      
+      <View style={styles.pass}>
       <TextInput
         style={styles.inputField}
         name="password"
@@ -62,13 +68,15 @@ return (
         textContentType="newPassword"
         secureTextEntry={passwordVisibility}
         value={password}
-        enablesReturnKeyAutomatically
+        // enablesReturnKeyAutomatically
         onChangeText={text => setPassword(text)}
+        placeholderTextColor = "#0096C7"
       />
       <Pressable onPress={handlePasswordVisibility}>
-        <MaterialCommunityIcons name={rightIcon} size={22} color="#232323" />
+        <MaterialCommunityIcons name={rightIcon} size={22} color='#016AFB' />
       </Pressable>
-
+      </View>
+     
       </View>
 
       <SelectList 
@@ -84,8 +92,23 @@ return (
         save="value"
         placeholder='Gender'
        />
-
       
+      <View>
+       <Checkbox value={agree} onValueChange={setAgree} color={'#016AFB'} />
+       <Text>Agree To Terms And Condition</Text>
+      </View>
+       
+       <Pressable>
+         <Text>Register</Text>
+       </Pressable>
+      
+      <View>
+       <Text> Already have an account? </Text>
+       <Pressable>
+         <Text>Sign In</Text>
+       </Pressable>
+      </View>
+       
   </View>
 );
 }
@@ -94,5 +117,39 @@ return (
 
 
 const styles = StyleSheet.create({
-
+ all : {
+  backgroundColor: 'white',
+  paddingTop: 100,
+ },
+ well : {
+  textAlign: 'center',
+  color: '#016AFB',
+ },
+ well1: {
+  fontWeight: 'bold',
+  textAlign: 'center',
+  color: '#016AFB',
+  paddingBottom: 20,
+ },
+ pass : {
+  flexDirection: 'row',
+  gap: 200,
+  justifyContent: 'space-around',
+  padding: 10,
+  borderBottomColor: '#016AFB',
+  borderBottomWidth: 1,
+  marginBottom: 20,
+  marginLeft: 35,
+  marginRight: 35,
+  marginTop: 20,
+ },
+ input : {
+  padding: 10,
+  borderBottomColor: '#016AFB',
+  borderBottomWidth: 1,
+  marginBottom: 20,
+  marginLeft: 35,
+  marginRight: 35,
+  marginTop: 20,
+ }
 });
