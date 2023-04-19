2f9f407b789fbfe0239179b660207c46656552c7
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-04-19 10:37:44 -0700

    first version

--- App.js
+++ App.js
@@ -24,38 +24,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // install react stack (npm install @react-navigation/native-stack)
 
 const Stack = createNativeStackNavigator();
-// const Tab = createBottomTabNavigator();
-
-
-// function TabNavigator() {
-//   return (
-//     <Tab.Navigator
-//     screenOptions={({ route }) => ({
-//       headerShown: false,
-//       tabBarIcon: ({ focused, color, size }) => {
-//         let iconName;
-
-//         if (route.name === 'Home1') {
-//           iconName = focused
-//             ? 'home'
-//             : 'home-outline';
-//         } else if (route.name === 'Settings') {
-//           iconName = focused ? 'ios-list' : 'ios-list-outline';
-//         }
-
-//         // You can return any component that you like here!
-//         return <Ionicons name={iconName} size={size} color={color} />;
-//       },
-//       tabBarActiveTintColor: 'tomato',
-//       tabBarInactiveTintColor: 'gray',
-//     })}
-//   >
-   
-//     <Tab.Screen name="Home1" component={StackNavigator}   />
-//     <Tab.Screen name="Give" component={Give}   />
-//   </Tab.Navigator>
-//   )
-// }
 
 
 
